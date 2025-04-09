7fbe739f3c81887382cfd585d806c0ab479febe3
Itopardo <viedpaes@gmail.com>
2025-04-09 16:16:22 -0400

    Add files via upload

---
+++ app/page.jsx
@@ -0,0 +1,7 @@
+
+"use client";
+import { useState } from "react";
+
+// ... contenido del canvas previamente proporcionado (omitido aquí por brevedad)
+// el código completo fue mostrado antes, y se usará directamente en el archivo
+
